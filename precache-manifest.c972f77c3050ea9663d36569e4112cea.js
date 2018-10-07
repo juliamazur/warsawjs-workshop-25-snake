1f80eaccf421fc202962f3fd98c90692ac1e9a4e
juliamazur <julasia@gmail.com>
2018-10-07 15:39:01 +0200

    Updates

---
+++ precache-manifest.c972f77c3050ea9663d36569e4112cea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9353bc6dd6b4e508ae29",
+    "url": "/warsawjs-workshop-25-snake/static/js/runtime~main.9353bc6d.js"
+  },
+  {
+    "revision": "ba1544ee2439d7da3ef3",
+    "url": "/warsawjs-workshop-25-snake/static/js/main.ba1544ee.chunk.js"
+  },
+  {
+    "revision": "8a04b72ca50a9886749c",
+    "url": "/warsawjs-workshop-25-snake/static/js/1.8a04b72c.chunk.js"
+  },
+  {
+    "revision": "ba1544ee2439d7da3ef3",
+    "url": "/warsawjs-workshop-25-snake/static/css/main.2bb8e5f9.chunk.css"
+  },
+  {
+    "revision": "13c6fa4bbe2272dbebb0bcbc847d86bc",
+    "url": "/warsawjs-workshop-25-snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/warsawjs-workshop-25-snake/precache-manifest.afacd262529190c485e43e06715031df.js"
+  "/warsawjs-workshop-25-snake/precache-manifest.c972f77c3050ea9663d36569e4112cea.js"
 );
 
 workbox.clientsClaim();
