8a9b1f92631dd68e69fae2853ada1e82e6ec1a37
juliamazur <julasia@gmail.com>
2018-10-07 18:02:06 +0200

    Updates

--- precache-manifest.5de79be0fc5f087cc61a3e742402f96d.js
+++ precache-manifest.5de79be0fc5f087cc61a3e742402f96d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/warsawjs-workshop-25-snake/static/js/runtime~main.9353bc6d.js"
   },
   {
-    "revision": "0448e77ae01f249345b1",
-    "url": "/warsawjs-workshop-25-snake/static/js/main.0448e77a.chunk.js"
+    "revision": "0c8b9879d69676c96751",
+    "url": "/warsawjs-workshop-25-snake/static/js/main.0c8b9879.chunk.js"
   },
   {
     "revision": "8a04b72ca50a9886749c",
     "url": "/warsawjs-workshop-25-snake/static/js/1.8a04b72c.chunk.js"
   },
   {
-    "revision": "0448e77ae01f249345b1",
+    "revision": "0c8b9879d69676c96751",
     "url": "/warsawjs-workshop-25-snake/static/css/main.b884ecfe.chunk.css"
   },
   {
-    "revision": "654315288989af6537ee0e354782ec82",
+    "revision": "133e431e5c615c0f04d8a4840767d691",
     "url": "/warsawjs-workshop-25-snake/index.html"
   }
 ];
