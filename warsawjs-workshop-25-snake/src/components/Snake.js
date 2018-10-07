9fcd7b590f5a8e5d369d853c002b796e0bd15579
juliamazur <julasia@gmail.com>
2018-10-07 15:40:02 +0200

    dziala, nie zabija sie

---
+++ warsawjs-workshop-25-snake/src/components/Snake.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Snake extends React.Component {
+
+
+
+    moveDown = () => {
+        this.setPosition(this.state.cx, this.state.cy + 10);
+    }
+
+    setPosition = (cx, cy) => {
+        this.setState({
+            cx: cx,
+            cy: cy,
+        });
+    }
+
+
+
+    render(){
+        return (
+            this.props.snakeSegments.map((segment, index) => {
+                return (
+                    <circle key={index} cx={segment.cx} cy={segment.cy} r={this.props.r} fill="lightgreen" />
+                )
+              })
+        );
+
+    }
+
+}
+
+export default Snake;
